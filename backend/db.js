5117e20d3f3d8a99ee586d63c627265e95ff9b6f
swapyshelke <swapyshelke02gmail.com>
2024-03-05 22:04:20 +0530

    security: env updated

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,12 @@
 //  - desriptoin: string
 //  - completed: boolean
 
-
+import 'dotenv/config'
 
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-mongoose.connect("mongodb+srv://swapy:swapnil@cluster0.crjtflw.mongodb.net/todos")
+mongoose.connect(process.env.DATAbASE_URL)
 
 const todoSchema = new Schema({
     title :String,
